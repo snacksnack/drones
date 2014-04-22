1ed5f9ab742d2d6d27b45e73c5a234c4eba8feb2
Reid Collins <velopants@gmail.com>
2014-04-22 10:27:29 -0400

    fix indentation in mongoid.yml. one more time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
+  require 'rails/mongoid'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
